b3b784e7529754b81924e506a726d907fefef5ec
catherine <wachukimani@gmail.com>
2023-03-10 16:38:16 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,37 @@
+
+function add(num1, num2) {
+    return num1 + num2;
+  }
+ 
+  function subtract(num1, num2) {
+    return num1 - num2;
+  }
+  
+  function multiply(num1, num2) {
+    return num1 * num2;
+  }
+  
+  function divide(num1, num2) {
+    return num1 / num2;
+  }
+
+  function increment(n) {
+    return n + 1;
+  }
+  
+  function decrement(n) {
+    return n - 1;
+  }
+
+  let n = 355;
+  function makeInt(n) {
+    return n;
+  }
+
+  function preserveDecimal(n=2.222) {
+    return parseFloat(n);
+  }
+
+  function makeInt(n) {
+    return parseInt(n, 10);
+  }
